394d000621cc6d23bc2a008bd2a5bf90efcea04e
tmegha1405 <117816288+tmegha1405@users.noreply.github.com>
2022-12-12 14:15:45 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+num=int(input("Enter the value:"))
+print("The fibonacci series is:")
+def fibonacci(num):
+    n1=0
+    n2=1
+    count=0
+    print(n1)
+    print(n2)
+    while(count<num-2):
+         n3=n1+n2
+         print(n3)
+         n1=n2
+         n2=n3
+         count=count+1
+fibonacci(num)
