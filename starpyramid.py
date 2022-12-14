0bb7517d80c7b50cb5caca4c7f91bfe6b05ef8b9
tmegha1405 <117816288+tmegha1405@users.noreply.github.com>
2022-12-14 10:17:59 +0530

    Add files via upload

---
+++ starpyramid.py
@@ -0,0 +1,9 @@
+n=int(input("Enter the no.of rows:"))
+for i in range(0,n):
+    for j in range(0,i+1):
+        print("*",end=" ")
+    print(" ")
+for i in range(n,0,-1):
+    for j in range(0,i):
+        print("*",end=" ")
+    print("")
