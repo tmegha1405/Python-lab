b9826b7b8bc2a3026f33aec873bc5f80a94d652b
tmegha1405 <117816288+tmegha1405@users.noreply.github.com>
2022-12-14 10:06:05 +0530

    Update numberpyramid.py

--- numberpyramid.py
+++ numberpyramid.py
@@ -4,3 +4,12 @@ for i in range(1,rows+1):
         square=i*j
         print(i*j,end=" ")
     print(" ")
+    
+    //
+    o/p
+Enter no.of rows:5
+1  
+2 4  
+3 6 9  
+4 8 12 16  
+5 10 15 20 25  
