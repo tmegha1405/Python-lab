a543bf1d8131a595291fd396f52b070eb3c7beb9
tmegha1405 <117816288+tmegha1405@users.noreply.github.com>
2022-12-14 10:19:47 +0530

    Update starpyramid.py

--- starpyramid.py
+++ starpyramid.py
@@ -7,3 +7,17 @@ for i in range(n,0,-1):
     for j in range(0,i):
         print("*",end=" ")
     print("")
+    
+    //
+    o/p
+    Enter the no.of rows:5
+*  
+* *  
+* * *  
+* * * *  
+* * * * *  
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
