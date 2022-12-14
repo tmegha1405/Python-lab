57bae82441b2ecedd40c38326ab8b318f07a8cfb
tmegha1405 <117816288+tmegha1405@users.noreply.github.com>
2022-12-14 10:38:08 +0530

    Update charcterfrequency.py

--- charcterfrequency.py
+++ charcterfrequency.py
@@ -5,3 +5,8 @@ def charFreq(str):
     return wordCount
 word=input("Rnter a string to check for character frequency:")
 print(charFreq(word))
+
+//
+o/p
+Enter a string to check for character frequency:mgha
+{'h': 1, 'm': 1, 'a': 1, 'g': 1}
