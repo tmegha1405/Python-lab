d66dfcf0bda179b2f3ec6fdf80ea961d6a1d4c6d
tmegha1405 <117816288+tmegha1405@users.noreply.github.com>
2022-12-19 14:47:14 +0530

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -8,3 +8,11 @@ b=int(input("Enter a number:"))
 print( "Area of square is:",area_square(a))
 print("Area of rectangle is:",area_rectangle(a,b))
 print("Area of triangle is:",area_triangle(a,b))
+
+//
+o/p
+Enter a number:10
+Enter a number:20
+Area of square is: 100
+Area of rectangle is: 200
+Area of triangle is: 100.0
